1a3884717c2f6ff696f252acd6720b3e70c1745d
Friedrich W. H. Kossebau <kossebau@kde.org>
2025-10-29 00:13:55 +0100

    ECMAddTests: add functions and variables to preset test name prefix

--- modules/ECMAddTests.cmake
+++ modules/ECMAddTests.cmake
@@ -1,5 +1,6 @@
 # SPDX-FileCopyrightText: 2013 Alexander Richardson <arichardson.kde@gmail.com>
 # SPDX-FileCopyrightText: 2015 Alex Merry <alex.merry@kde.org>
+# SPDX-FileCopyrightText: 2025 Friedrich W. H. Kossebau <kossebau@kde.org>
 #
 # SPDX-License-Identifier: BSD-3-Clause
 
@@ -34,6 +35,9 @@ If ``NAME_PREFIX`` is given, this prefix will be prepended to the test names, bu
 not the target names. As a result, it will not prevent clashes between tests
 with the same name in different parts of the project, but it can be used to
 give an indication of where to look for a failing test.
+If ``NAME_PREFIX`` is not set, it will default to a value depending on the
+strategy as controlled by the variable ``ECM_TEST_NAME_PREFIX_STRATEGY``, if
+set in the current scope (since 6.21).
 
 If the flag ``GUI`` is passed the test binaries will be GUI executables, otherwise
 the resulting binaries will be console applications (regardless of the value
@@ -73,14 +77,107 @@ generator expressions. Since 5.111.
 
 This is a single-test form of ``ecm_add_tests`` that allows multiple source files
 to be used for a single test. If using multiple source files, ``TEST_NAME`` must
-be given; this will be used for both the target and test names (and, as with
-``ecm_add_tests()``, the ``NAME_PREFIX`` argument will be prepended to the test name).
+be given; this will be used for both the target and test names.
+
+As with ``ecm_add_tests()``, the ``NAME_PREFIX`` argument will be prepended to
+the test name.
+If ``NAME_PREFIX`` is not set, it will default to a value depending on the
+strategy as controlled by the variable ``ECM_TEST_NAME_PREFIX_STRATEGY``, if
+set in the current scope (since 6.21).
 
 ``WORKING_DIRECTORY`` sets the test property `WORKING_DIRECTORY
 <https://cmake.org/cmake/help/latest/prop_test/WORKING_DIRECTORY.html>`_
 in which to execute the test. By default the test will be run in
 ``${CMAKE_CURRENT_BINARY_DIR}``. The working directory can be specified using
 generator expressions. Since 5.111.
+
+::
+
+  ecm_test_set_dir_properties( # Since 6.21.0
+      [DIR <dir>]
+      [PREFIX_NAME <name>]
+      [PREFIX_NAME_IGNORE | PREFIX_NAME_NOT_IGNORE]
+      [FIRST_PREFIX_NAME]
+      [LAST_PREFIX_NAME]
+  )
+
+
+The function can be used to set properties to individual directories to
+influence the behaviour of the test functions. The properties will be set to
+the current source directory, unless the ``DIR`` argument is passed to target
+another directory. The ``<dir>`` value can also refer to a source directory
+which itself does not have a CMakeLists.txt file and thus is not added by any
+`add_subdirectory()
+<https://cmake.org/cmake/help/latest/command/add_subdirectory.html>`_ call.
+
+``PREFIX_NAME`` can be used to override by ``<name>`` the name derived from
+the given directory.
+
+``PREFIX_NAME_IGNORE`` or ``PREFIX_NAME_NOT_IGNORE`` can be used to override
+the filtering done by ``ECM_TEST_NAME_PREFIX_IGNORE_DIRS`` for the name of the
+given directory.
+
+``FIRST_PREFIX_NAME`` and ``LAST_PREFIX_NAME`` can be used to limit the range
+of dirs in the path used for deriving the prefix name.
+
+::
+
+  ecm_test_get_name_prefix( # Since 6.21.0
+      <name_prefix_var>
+  )
+
+The ``name_prefix_var`` argument should specify a variable name to receive the
+default test prefix name for the current scope, based on
+the value of ``ECM_TEST_NAME_PREFIX_STRATEGY`` and the respective further variables.
+
+::
+
+  ECM_TEST_NAME_PREFIX_STRATEGY # Since 6.21.0
+
+This variable is specifying the strategy to use when estimating the default
+test name prefix to use by the macros ``ecm_add_tests()`` and ``ecm_add_test()``
+when no explicit ``NAME_PREFIX`` argument is passed. The supported values are:
+
+    * ``VARIABLE``: The default name prefix is taken from the value of the variable
+      ``ECM_TEST_NAME_PREFIX`` in the scope where the macros is called.
+
+    * ``PATH``: The default name prefix is derived from the relative path of the
+      current source directory where the macro is called, by replacing the
+      dir separators with "-".
+      See also ``ECM_TEST_NAME_PREFIX_IGNORE_DIRS`` for skipping some directory names.
+
+When unset, the ``VARIABLE`` strategy is used.
+
+::
+
+  ECM_TEST_NAME_PREFIX # Since 6.21.0
+
+When ``ECM_TEST_NAME_PREFIX_STRATEGY`` is set to ``VARIABLE``,
+the current value of the variable is used as default for the test name prefix.
+
+::
+
+  ECM_TEST_NAME_PREFIX_IGNORE_DIRS # Since 6.21.0
+
+When ``ECM_TEST_NAME_PREFIX_STRATEGY`` is set to ``PATH``, this variable is used
+to filter out directory names in the relative path of the current source directory
+when deriving the name prefix. By default the varibale is set to
+"src", "test", "tests", "autotest", "autotests" when including the ``ECMAddTest`` module.
+
+
+Example usage:
+
+.. code-block:: cmake
+
+  set(ECM_TEST_NAME_PREFIX_STRATEGY "PATH")
+  list(APPEND ECM_TEST_NAME_PREFIX_IGNORE_DIRS "plugins")
+
+  # Being in subdir "src/plugins/foo" this test will get
+  # the test name prefix set to "foo-", next to base name "mytest":
+  ecm_add_test(
+      mytest.cpp
+      LINK_LIBRARIES mylib
+  )
 
 Since pre-1.0.0.
 #]=======================================================================]
@@ -89,6 +186,149 @@ cmake_policy(VERSION 3.16)
 
 include(ECMMarkAsTest)
 include(ECMMarkNonGuiExecutable)
+
+if(NOT ECM_TEST_NAME_PREFIX_IGNORE_DIRS)
+    set(ECM_TEST_NAME_PREFIX_IGNORE_DIRS "src" "test" "tests" "autotest" "autotests")
+endif()
+
+function(_ecm_test_name_prefix_from_project_path _name_prefix_varname)
+    # estimate names of chain of dirs of current source dir to main dir
+    file(RELATIVE_PATH _project_path ${CMAKE_SOURCE_DIR} ${CMAKE_CURRENT_SOURCE_DIR})
+
+    if(_project_path MATCHES "^\\.\\./")
+        message(FATAL_ERROR "The PATH strategy can not be used with directories outside the root source dir.")
+    endif()
+
+    string(REPLACE "/" ";" _dirs ${_project_path})
+
+    # derive prefix names
+    set(_names)
+    set(_subpath ${CMAKE_SOURCE_DIR})
+    set(_lastcmakesubpath ${_subpath}) # path of current including CMakeLists.txt
+    set(_relativelastcmakesubpath)
+    get_directory_property(_cmakesubdirs DIRECTORY ${_lastcmakesubpath} "SUBDIRECTORIES")
+    foreach(_dir ${_dirs})
+        string(APPEND _subpath "/${_dir}")
+
+        # subdir with CMakeLists.txt?
+        if(_subpath IN_LIST _cmakesubdirs)
+            set(_lastcmakesubpath ${_subpath})
+            set(_relativelastcmakesubpath)
+            set(_property_name_suffix)
+            # prepare nexs subdir
+            get_directory_property(_cmakesubdirs DIRECTORY ${_lastcmakesubpath} "SUBDIRECTORIES")
+        else()
+            string(APPEND _relativelastcmakesubpath "/${_dir}")
+            string(SHA1 _relativelastcmakesubpath_hash ${_relativelastcmakesubpath})
+            set(_property_name_suffix ":${_relativelastcmakesubpath_hash}")
+        endif()
+
+        get_directory_property(_substitute_name DIRECTORY ${_lastcmakesubpath} "ECM_TEST_NAME_PREFIX_DIR_NAME${_property_name_suffix}")
+
+        get_directory_property(_first_name DIRECTORY ${_lastcmakesubpath} "ECM_TEST_NAME_PREFIX_DIR_FIRST${_property_name_suffix}")
+
+        if(_first_name)
+            # drop any current names
+            set(_names)
+        endif()
+
+        if (_substitute_name)
+            list(APPEND _names ${_substitute_name})
+        else()
+            get_directory_property(_ignore_dir DIRECTORY ${_lastcmakesubpath} "ECM_TEST_NAME_PREFIX_DIR_IGNORE${_property_name_suffix}")
+            if (_ignore_dir STREQUAL "")
+                if(_dir IN_LIST ECM_TEST_NAME_PREFIX_IGNORE_DIRS)
+                    set(_ignore_dir TRUE)
+                else()
+                    set(_ignore_dir FALSE)
+                endif()
+            endif()
+            if(NOT _ignore_dir)
+                list(APPEND _names ${_dir})
+            endif()
+        endif()
+
+        get_directory_property(_last_name DIRECTORY ${_lastcmakesubpath} "ECM_TEST_NAME_PREFIX_DIR_LAST${_property_name_suffix}")
+        if(_last_name)
+            # finish estimating prefix names
+            break()
+        endif()
+    endforeach()
+
+    # create final prefix name
+    string(JOIN "-" _name_prefix ${_names})
+    if (_name_prefix)
+        string(APPEND _name_prefix "-")
+    endif()
+
+    set(${_name_prefix_varname} ${_name_prefix} PARENT_SCOPE)
+endfunction()
+
+function(ecm_test_set_dir_properties)
+    set(options PREFIX_NAME_IGNORE PREFIX_NAME_NOT_IGNORE FIRST_PREFIX_NAME LAST_PREFIX_NAME)
+    set(oneValueArgs PREFIX_NAME DIR)
+    set(multiValueArgs)
+    cmake_parse_arguments(ARG "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
+
+    if(ARG_UNPARSED_ARGUMENTS)
+        message(FATAL_ERROR "Unknown keywords given to ECM_TEST_SET_DIR_PROPERTIES(): \"${ARG_UNPARSED_ARGUMENTS}\"")
+    endif()
+    if(ARG_PREFIX_NAME_IGNORE AND ARG_PREFIX_NAME_NOT_IGNORE)
+        message(FATAL_ERROR "Pass either PREFIX_NAME_IGNORE or PREFIX_NAME_NOT_IGNORE to ECM_TEST_SET_DIR_PROPERTIES.")
+    endif()
+    set(_subdir)
+    if(ARG_DIR)
+        get_filename_component(_subdir ${ARG_DIR} REALPATH)
+        if(NOT IS_DIRECTORY "${_subdir}")
+            message(FATAL_ERROR "DIR argument passed to ECM_TEST_SET_DIR_PROPERTIES is not a directory: \"${ARG_DIR}\"")
+        endif()
+        if (_subdir STREQUAL ${CMAKE_CURRENT_SOURCE_DIR})
+            set(_subdir)
+        endif()
+    endif()
+
+    if(_subdir)
+        file(RELATIVE_PATH _rel_path ${CMAKE_CURRENT_SOURCE_DIR} ${_subdir})
+        string(SHA1 _rel_path_hash ${_rel_path})
+        set(_property_name_suffix ":/${_rel_path_hash}")
+    else()
+        set(_property_name_suffix)
+    endif()
+
+    if (ARG_PREFIX_NAME)
+        set_directory_properties(PROPERTIES "ECM_TEST_NAME_PREFIX_DIR_NAME${_property_name_suffix}" "${ARG_PREFIX_NAME}")
+    endif()
+    if (ARG_PREFIX_NAME_IGNORE)
+        set_directory_properties(PROPERTIES "ECM_TEST_NAME_PREFIX_DIR_IGNORE${_property_name_suffix}" "TRUE")
+    endif()
+    if (ARG_PREFIX_NAME_NOT_IGNORE)
+        set_directory_properties(PROPERTIES "ECM_TEST_NAME_PREFIX_DIR_IGNORE${_property_name_suffix}" "FALSE")
+    endif()
+    if (ARG_FIRST_PREFIX_NAME)
+        set_directory_properties(PROPERTIES "ECM_TEST_NAME_PREFIX_DIR_FIRST${_property_name_suffix}" "TRUE")
+    endif()
+    if (ARG_LAST_PREFIX_NAME)
+        set_directory_properties(PROPERTIES "ECM_TEST_NAME_PREFIX_DIR_LAST${_property_name_suffix}" "TRUE")
+    endif()
+endfunction()
+
+function(ecm_test_get_name_prefix _name_prefix_varname)
+    if(NOT ECM_TEST_NAME_PREFIX_STRATEGY)
+        set(ECM_TEST_NAME_PREFIX_STRATEGY "VARIABLE")
+    else()
+        set(_strategies "VARIABLE" "PATH")
+        if (NOT ECM_TEST_NAME_PREFIX_STRATEGY IN_LIST _strategies)
+            message(FATAL_ERROR "ECM_TEST_NAME_PREFIX_STRATEGY set to unknown value ${ECM_TEST_NAME_PREFIX_STRATEGY}")
+        endif()
+    endif()
+
+    if (${ECM_TEST_NAME_PREFIX_STRATEGY} STREQUAL "PATH")
+        _ecm_test_name_prefix_from_project_path(_name_prefix)
+    else()
+        set(_name_prefix ${ECM_TEST_NAME_PREFIX})
+    endif()
+    set(${_name_prefix_varname} ${_name_prefix} PARENT_SCOPE)
+endfunction()
 
 function(ecm_add_test)
   set(options GUI)
@@ -109,6 +349,9 @@ function(ecm_add_test)
     message(FATAL_ERROR "ecm_add_test() called with multiple source files but without setting \"TEST_NAME\"")
   endif()
 
+  if (NOT ARG_NAME_PREFIX)
+      ecm_test_get_name_prefix(ARG_NAME_PREFIX)
+  endif()
   set(_testname ${ARG_NAME_PREFIX}${_targetname})
   set(gui_args)
   if(ARG_GUI)
